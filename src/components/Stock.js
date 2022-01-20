0fdecf9e393c9182fe63b88ec1eb72cf26e08b10
Ryan Small <ryan.small@feapc.com>
2022-01-19 23:15:57 -0500

    fixing Stock component

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -4,8 +4,11 @@ const Stock = props => {
   const stock = props.data.filter( d => d.symbol === props.match.params.symbol)[0]
   return (
     <div>
-      <h3>Name: {stock.name}</h3>
-      <h3 className="price">Price: {stock.lastPrice}</h3>
+      <h2>Name: {stock.name}</h3>
+      <h3>Stock Symbol: {stock.symbol}</h2>
+      <h3>Price: {stock.lastPrice}</h2>
+      <h3>High: {stock.high}</h2>
+      <h3>Low: {stock.low}</h2>
     </div>
   )
 }
